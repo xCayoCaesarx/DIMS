db244ff7845f00fd9a79091ec42c329a788599ce
juzcategui <juzcategui@juzcategui>
2022-05-06 13:04:14 -0400

    fix bulk reassign

---
+++ Test Suites/Bulk Reassign.ts
@@ -0,0 +1,41 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Bulk Reassign</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>3b480b19-9524-4b82-bad7-d982142dd581</testSuiteGuid>
+   <testCaseLink>
+      <guid>6cce55a3-56a1-40f8-8fb9-a6523b17468b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Reassign Leads</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>9cc60f45-ee37-4ac5-b947-362a778f4ba0</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Mailer Data</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>9cc60f45-ee37-4ac5-b947-362a778f4ba0</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>username</value>
+         <variableId>3372bd4a-7a75-49ec-be0a-a237459783dd</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>9cc60f45-ee37-4ac5-b947-362a778f4ba0</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>pass</value>
+         <variableId>88209134-7d32-44f5-a98c-d67c954de2ac</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
